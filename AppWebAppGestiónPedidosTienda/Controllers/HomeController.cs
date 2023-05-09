b02603c40b36fdb88a32a565adbbccf418259e03
Brandon Emerson <111790942+Brandon2003-dev@users.noreply.github.com>
2023-05-08 22:19:53 -0600

    Add files via upload

---
+++ AppWebAppGestiónPedidosTienda/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using AppWebAppGestiónPedidosTienda.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AppWebAppGestiónPedidosTienda.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Program()
+        {
+            AppGestiónPedidosTiendaContext _contex = new AppGestiónPedidosTiendaContext();
+            var ListarProductos = _contex.Productos.ToList();
+
+            //ViewBag.Productos = ListarProductos;
+
+            return View(ListarProductos);
+            
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AppWebAppGestiónPedidosTienda/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppWebAppGestiónPedidosTienda.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
